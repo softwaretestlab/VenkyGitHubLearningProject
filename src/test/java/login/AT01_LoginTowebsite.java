6d2b2985d93993de2c85d540c14c6d7d435a5cf2
softwaretestlab <softwaretestlab@hotmail.com>
2023-03-24 18:39:16 +1100

    login

---
+++ src/test/java/login/AT01_LoginTowebsite.java
@@ -0,0 +1,13 @@
+package login;
+
+import org.junit.Test;
+
+import javax.sound.midi.Soundbank;
+
+public class AT01_LoginTowebsite {
+
+    @Test
+    public void test01(){
+        System.out.println("This is method 1");
+    }
+}
